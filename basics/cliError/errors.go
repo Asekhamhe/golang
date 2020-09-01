236a47c04cb6341747e22962a055a85a3bbe2417
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2020-09-01 17:47:49 +0100

    basic programming and a dockerized golang container

---
+++ basics/cliError/errors.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Please gie one or more numbers.")
+		os.Exit(1)
+	}
+
+	var err error = errors.New("an error")
+	var number float64
+
+	arguments := os.Args
+	index := 1
+
+	for err != nil {
+		if index >= len(arguments) {
+			fmt.Println("None of the arguments is a float!")
+			return
+		}
+		number, err = strconv.ParseFloat(arguments[index], 64)
+		index++
+	}
+
+	min, max := number, number
+
+	for i := 2; i < len(arguments); i++ {
+		number, err := strconv.ParseFloat(arguments[i], 64)
+		if err == nil {
+			if number < min {
+				min = number
+			}
+			if number > max {
+				max = number
+			}
+		}
+	}
+
+	fmt.Println("Min: ", min)
+	fmt.Println("Max: ", max)
+
+}
